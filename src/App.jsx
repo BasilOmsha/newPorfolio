fa8624dedda645cb62d88a4ae67e83de1a8a0f3e
Basel <basil.omsha@gmail.com>
2025-04-12 13:17:18 +0300

    init code

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <main>
+            <div>
+                <h1>Welcome to My portfolio</h1>
+            </div>
+        </main>
+    )
+}
+
+export default App
