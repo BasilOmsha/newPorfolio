a54e5f0dfebce051fc7d81f774ac8d06a580b64d
Basel <basil.omsha@gmail.com>
2025-04-14 14:01:21 +0300

    fix: correct typo in class names from 'grading-edge' to 'gradient-edge'

--- src/sections/LogoSection.jsx
+++ src/sections/LogoSection.jsx
@@ -4,8 +4,8 @@ import { logoIconsList } from "../constants/index.js"
 function LogoSection() {
     return (
         <div className="md:my-20 my-10 relative">
-            <div className="grading-edge" />
-            <div className="grading-edge" />
+            <div className="gradient-edge" />
+            <div className="gradient-edge" />
 
             <div className="marquee h-52">
                 <div className="marquee-box md:gap-12 gap-5">
