9743c513d7be978c0cef70ce38138ac25923fdc7
Basel <basil.omsha@gmail.com>
2025-04-19 01:23:48 +0300

    fix: update Vite configuration for server and build settings

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss()],
+    server: {
+        host: true // Open to local network and display URL
+        // open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env) // Open if it's not a CodeSandbox
+    },
+    build: {
+        outDir: "../dist", // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    }
 })
